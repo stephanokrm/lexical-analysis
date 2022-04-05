68f8423b8d5b3209a00b367fd3ed44f27e6dc072
Brenda <brendabarbosa@rede.ulbra.br>
2022-04-05 18:08:52 -0300

    Adding comments

--- src/analyze.ts
+++ src/analyze.ts
@@ -4,10 +4,12 @@ import expressions from "./expressions";
 const identify = (expression: Expression, content: string, line: number, symbols: Symbols): string => {
     const token = expression.token;
 
+    //retorna identificadores e constantes numéricas
     if (expression.isSymbol) {
         return `[${line}] ${token} ${symbols[content]}`;
     }
 
+    //retorna palavras reservadas
     if (expression.isReserved) {
         return `[${line}] ${token[content]}`;
     }
@@ -28,19 +30,23 @@ const analyse = (fileContent: string): Analysis => {
     const tokens = [];
     const symbols = {};
 
+    //percorre as linhas do arquivo
     for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
         let identified = false;
         const lineContent = lines[lineIndex];
         const line = lineIndex + 1;
 
+        //percorre as expressões para identificar o tipo de token
         for (let expressionIndex = 0; expressionIndex < expressions.length; expressionIndex++) {
             const expression = expressions[expressionIndex];
 
+            //testa se o token corresponde a expressão regular
             if (expression.pattern.test(lineContent)) {
                 if (expression.isSymbol && !symbols.hasOwnProperty(lineContent)) {
                     symbols[lineContent] = Object.keys(symbols).length + 1;
                 }
 
+                //identifica o tipo do token
                 const result = identify(expression, lineContent, line, symbols);
 
                 tokens.push(result);
@@ -51,6 +57,7 @@ const analyse = (fileContent: string): Analysis => {
             }
         }
 
+        //armazena erro caso token não seja identificado
         if (!identified) {
             errors.push(`${line} (${lineContent})`);
         }
